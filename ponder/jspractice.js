784e792dd4b5e8221110ac1130c21693931e039f
sethwboss <sethwboss@gmail.com>
2025-04-28 14:16:16 -0600

    added js practice in ponder

---
+++ ponder/jspractice.js
@@ -0,0 +1,48 @@
+const PI = 3.14;
+
+let radius = 3;
+
+let area = radius * radius * PI;
+
+
+console.log(area);
+
+
+radius = 20;
+
+area = radius * radius * PI;
+
+console.log(area);
+
+
+const one = 1;
+const two = '2';
+
+let result = one * two;
+
+console.log(result);
+
+
+result = one + two;
+
+console.log(result);
+
+
+
+
+
+
+
+
+let globalVariable = "I'm global";
+
+function myFunction() {
+    let blockVariable = "I am local or block level";
+    console.log(blockVariable);
+}
+
+
+console.log(globalVariable);
+
+
+myFunction();
