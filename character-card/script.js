582fc241be8cbbe423b248b67d4deccf40c7ca60
sethwboss <sethwboss@gmail.com>
2025-06-10 10:04:34 -0600

    Finished character card

--- character-card/script.js
+++ character-card/script.js
@@ -17,3 +17,35 @@ const character = {
         this.health += 20;
       }
     };
+
+
+/* For when the page loads*/
+document.querySelector('.image').src = character.image;
+document.querySelector('.name').textContent = character.name;
+
+function updateCharacterCard() {
+  document.querySelector('#class').textContent = character.class;
+  document.querySelector('#level').textContent = character.level;
+  document.querySelector('#health').textContent = character.health;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    updateCharacterCard(); // Call the update function when the DOM is ready
+});
+
+
+  
+
+document.querySelector('#attacked').addEventListener('click', function(){
+    character.attacked();
+
+    // Update Stats
+    updateCharacterCard()
+})
+
+document.querySelector('#levelup').addEventListener('click', function(){
+    character.levelUp();
+
+    //Update Stats
+    updateCharacterCard()
+})
